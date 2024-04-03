57049828a3e506e232310cc9f609742836dbd212
Amit Kumar <116919706+AmitKumarRay-ak@users.noreply.github.com>
2024-04-03 18:46:25 +0530

    yes

--- DSA/1 variables and datatypes/basic.java
+++ DSA/1 variables and datatypes/basic.java
@@ -3,5 +3,6 @@ public class basic {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("AMIT");
+        System.out.println("Code With Amit");
     }
 }
